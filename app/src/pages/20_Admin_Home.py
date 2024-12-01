b1f638feef1a8b47ee2397605719f6307fd056ef
Mark Fontenot <m.fontenot@northeastern.edu>
2024-12-01 06:14:28 -0500

    Update the Admin Home Page to have a button for the Add Product page

--- app/src/pages/20_Admin_Home.py
+++ app/src/pages/20_Admin_Home.py
@@ -15,3 +15,8 @@ if st.button('Update ML Models',
              type='primary',
              use_container_width=True):
   st.switch_page('pages/21_ML_Model_Mgmt.py')
+  
+if st.button('Add New Product',
+             type = 'primary',
+             use_container_width=True):
+    st.switch_page('pages/22_Add_Product.py')
